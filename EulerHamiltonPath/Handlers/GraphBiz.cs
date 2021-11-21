ba28aecff4230f5e145005898f11253368284993
nmt-ht <nmt.hitu@gmail.com>
2021-11-21 22:58:34 +0700

    [TN] - Init a project

---
+++ EulerHamiltonPath/Handlers/GraphBiz.cs
@@ -0,0 +1,27 @@
+﻿using Models;
+
+namespace EulerHamiltonPath.Handlers
+{
+    public static class GraphBiz
+    {
+        public static  bool IsEulerPath(AdjacencyMatrix adjacencyMatrix)
+        {
+            return false;
+        }
+
+        public static bool IsHamiltonPath(AdjacencyMatrix adjacencyMatrix)
+        {
+            return false;
+        }
+
+        public static bool IsConnectedGraph(AdjacencyMatrix adjacencyMatrix)
+        {
+            return true;
+        }
+
+        private static void DFS(int v, bool[] visited, AdjacencyMatrix adjacencyMatrix)
+        {
+            
+        }
+    }
+}
---
+++ EulerHamiltonPath/Program.cs
@@ -0,0 +1,16 @@
+﻿using EulerHamiltonPath.Helpers;
+using System;
+
+namespace EulerHamiltonPath
+{
+    class Program
+    {
+        private const string ADJACENCY_MATRIX_INPUT_FILE = @".\Source\input.txt";
+        static void Main(string[] args)
+        {
+            var matrix = Helper.InitAdjacencyMatrix(ADJACENCY_MATRIX_INPUT_FILE);
+            Helper.PrintMatrixToScreen(matrix);
+            Console.WriteLine();
+        }
+    }
+}
